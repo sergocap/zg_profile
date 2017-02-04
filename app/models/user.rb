3cb48ff83419574662ac5cda4240b0598290be9b
sergocap <systemofadown.2013@yandex.ru>
2017-02-04 18:54:55 +0700

    add redis_session_store

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :timeoutable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def after_database_authentication
+    RedisUserConnector.set(id, info.to_a.flatten)
+  end
+
+  def info
+    {}.tap do |ui|
+      ui['name'] = name
+      ui['email'] = email
+      ui['sign_in_count'] = sign_in_count
+      ui['last_sign_in_at'] = last_sign_in_at.to_s
+      ui['created_at'] = created_at
+      ui['timeout_in'] = timeout_in.seconds.to_s
+    end
+  end
+
+  def redis_info
+    RedisUserConnector.get(id)
+  end
 end
