e3b6f4947c1708cf92b8f29f12199dc13ac52c41
Serega <glmiyarov@bk.ru>
2017-02-10 12:14:02 +0700

    local2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025  }
+  config.action_mailer.raise_delivery_errors = false
 end
