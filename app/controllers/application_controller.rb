2cff634fc5727d5993780c5d416d33d5f55cb655
sergocap <systemofadown.2013@yandex.ru>
2017-02-27 23:34:27 +0700

    redirect to back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_redirect_url
   skip_before_action :verify_authenticity_token, only: [:create, :destroy]
 
   protected
@@ -8,5 +9,19 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :patronymic])
     devise_parameter_sanitizer.permit(:account_update, keys: [:surname, :name, :patronymic])
+  end
+
+  def set_redirect_url
+    session['redirect_url'] ||= params['redirect_url']
+  end
+
+  def after_sign_in_path_for(resource)
+    url = session.delete('redirect_url') || super
+    url || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    url = params['redirect_url'] || super
+    url || root_path
   end
 end
