4167fa15adc15d9f19ce88f8b1304424d0ee9f42
sergocap <systemofadown.2013@yandex.ru>
2017-03-21 12:21:19 +0700

    add manage denied flash

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -1,6 +1,10 @@
 class Manage::ApplicationController < ActionController::Base
   layout 'manage'
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, flash: { denied: 'Недостаточно прав для выполнения операции' }
+  end
+
   def namespace
     @current_namespace ||= controller_path.split('/').first.capitalize
   end
